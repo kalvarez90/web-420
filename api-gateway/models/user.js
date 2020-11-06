dd685e3d8a350c22dc5790f06dd83e5d6dd3850f
Karina <alvarez-webdev@gmail.com>
2020-11-06 17:12:22 -0500

    commit

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -16,4 +16,15 @@ var userSchema = new mongoose.Schema({
     email: String
 })
 
-module.exports = mongoose.model('User', userSchema);
+var User = module.exports = mongoose.model('User', userSchema);
+
+//user .save is used to add a new user in the database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+}
+
+//getById
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query,callback);
+};
