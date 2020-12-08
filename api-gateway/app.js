449ddee06b308e6038d48bdc9519592eab79aadf
Karina <alvarez-webdev@gmail.com>
2020-12-08 15:53:19 -0500

    commit

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -46,7 +46,9 @@ app.use('/api', apiCatalog); //part ll
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
